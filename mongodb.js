7efe6be965f2af20ba9559276357ef4aad838825
Ana HG <anaisabel.hortiguela@gmail.com>
2020-01-24 16:10:54 +0000

    First steps with Mongoose

--- mongodb.js
+++ mongodb.js
@@ -9,30 +9,124 @@ const databaseName = 'task-manager'
 // It creates an ID (5e29a50e3486320874fa827d)
 //const id = mongodb.ObjectID
 
-const id = new ObjectID()
-console.log(id)
+// const id = new ObjectID()
 
-// getTimestamp access the time in which the id was created. Please note that this is embedded in the first 4 caracteres of the id itself. ie:   5e29a7b224a18b0bd4b33adf 2020-01-23T14:03:30.000Z
-console.log(id.getTimestamp())
+// // To buffer/binary
+// console.log(id.id.length)
 
-MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error,client) => {
+// //Back to string
+// console.log(id.toHexString().length)
+
+// // getTimestamp access the time in which the id was created. Please note that this is embedded in the first 4 caracteres of the id itself. ie:   5e29a7b224a18b0bd4b33adf 2020-01-23T14:03:30.000Z
+// console.log(id.getTimestamp())
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error,client) => {
     if(error){
-        console.log('Unable to connect to database')
+        return console.log('Unable to connect to database')
     }
+const db = client.db(databaseName)
 
-    const db = client.db(databaseName)
 
-//     db.collection('users').insertOne({
-//         name: 'Andrew',
+    db.collection('tasks').deleteOne({
+        description: "Clean the house"
+    }).then((result)=>{
+        console.log(result)
+    }).catch((error)=>{
+        console.log(error)
+    })
+})
+//     db.collection('users').deleteMany({
 //         age: 27
-//     }, (error, result) => {
-//         if(error){
-//             return console.log('Unable to insert user')
-
-//         }
-//         console.log(result.ops)
+//     }).then((result)=> {
+//         console.log(result)
+//     }).catch((error)=>{
+//         console.log(error)
 //     })
 // })
+
+    
+
+//     db.collection('tasks').updateMany({
+//             completed: false
+//     }, {
+//         $set: {
+//             completed: true
+//         }
+
+//     }).then((result )=>{
+//         //modifiedCount reports the number of rows that have been modified
+//         console.log(result.modifiedCount)
+//     }).catch((error) =>{
+//         console.log(error)
+//     })
+// })
+
+
+
+
+    
+
+    // const updatePromise = db.collection('Users').updateOne({
+    //     _id: new ObjectID("5e298ac91dfe87288018f84c")
+    // },{
+    //     $inc:{
+    //         age: 10
+    //     }
+    //     // $set: {
+    //     //     name: 'Juanito'
+    //     // }
+    // })
+    // updatePromise.then((result) =>{
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+
+    // })
+
+
+
+    // db.collection('users').findOne({_id:new ObjectID("5e29aced74d406471c385add")}, (error, user) =>{
+    //     if(error){
+    //        return console.log('Unable to fetch')
+    // }
+    //     console.log(user)  
+    // })
+
+
+
+    // db.collection('tasks').findOne({_id:new ObjectID("5e29a1084f4857454c67cdad") }, (error, user) =>{
+    //     if(error){
+    //        return console.log(task)
+    // }
+    //     console.log(user)  
+    // })
+
+
+
+
+    //  db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+    //      console.log(tasks)
+    // })
+    // .find returns a cursor!!! So if I want a array I have to use the method toArray, for instance. 
+    // db.collection('users').find({age:27}).toArray((error, users) => {
+    // console.log(users)
+    // })
+
+
+
+
+    //  db.collection('users').insertOne({
+    //             name: 'Dorina',
+    //             age: 26
+    //  }, (error, result) => {
+    //      if(error){
+    //          return console.log('Unable to insert user')
+
+    //      }
+    //      console.log(result.ops)
+    //  })
+
+
 
     // db.collection('users').insertMany([
     //     {
@@ -52,6 +146,8 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error,client) => {
     // console.log(result.ops)
     // })
 
+
+
 //     db.collection('tasks').insertMany([
 //         {
 //              description: 'Clean the house',
@@ -65,14 +161,3 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error,client) => {
 //         description: 'Do grosery',
 //         completed: false
 
-
-    
-//    }], (error, result) => {
-//         if(error){
-//           return console.log('Unable to insert tasks')
-//         }
-//     //ops it is an array of the documents that I am sending
-//     console.log(result.ops)
-//     })
-
-})
