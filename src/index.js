784563806e0560f25023256abc87108c42a76e65
Ana HG <anaisabel.hortiguela@gmail.com>
2020-01-28 12:29:08 +0000

    Starting with Security

--- src/index.js
+++ src/index.js
@@ -1,153 +1,32 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/users')
-const Task = require('./models/tasks')
-
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+const router = new express.Router()
 
 const app = express()
 const port = process.env.PORT || 3000
 
-// It grab incoming data into JSON
+// It grabs incoming data into JSON
 app.use(express.json())
-
-
-app.post('/users', async (req,res) => {
-        const user = new User(req.body)
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch(e){
-            res.status(400).send(e)
-        }
-})
-
-app.get('/users', async (req, res) => {
-    try{   
-        const users =  await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try{
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-    }
-        res.send(user)
-    } catch(e) {
-        res.status(500).send()
-    }
-})
-   
-app.path('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if(!isValidOperation){
-        return res.status(404).send( {error : 'Invalid updats!'})
-    }
-    try {
-        const user = await User.findByIdAndDelete(req.params.id, req.body, {new: true, runValidators : true })
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    }catch (e){
-        res.status(400).send(e)
-
-    }
-})
-
-
-app.delete('/users/:id', async (req, res) => {
-    try{
-        const user = await User.findByIdAndDelete(req.params.id)
-        if(!user){
-            return res.status(400).send()
-        }
-
-    }catch(e){
-        res.status(500).send()
-
-    }
-})
-
-
-app.post('/tasks', async (req,res) => {
-        const task = new Task(req.body)
-        try {
-              await task.save()
-              res.status(201).send(task)
-        } catch(e) {
-        res.status(400).send(e)
-        }
-})
-
-
- app.get('/tasks', async (req, res) => {
-        try{   
-            const tasks =  await Task.find({})
-            res.send(tasks)
-        } catch (e) {
-            res.status(500).send()
-        }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    try{
-        const task = await Task.findById(_id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch( e) {
-        res.status(500).send()
-    }
-})
-
-app.path('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'priority']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if(!isValidOperation){
-        return res.status(404).send( {error : 'Invalid updats!'})
-    }
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id, req.body, {new: true, runValidators : true })
-        if(!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    }catch (e){
-        res.status(400).send(e)
-
-    }
-})
-
-
-
-app.delete('/tasks/:id', async (req, res) => {
-    try{
-        const task = await Task.findByIdAndDelete(req.params.id)
-        if(!task){
-            return res.status(400).send()
-        }
-
-    }catch(e){
-        res.status(500).send()
-
-    }
-})
-
+app.use(router)
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port,()=>{
     console.log('Server is up on port ' + port)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'floresRojas'
+    const hashedPassword = await bcrypt.hash(password, 8)
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('floreRojas', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
