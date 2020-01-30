90c138b26972c3ef05c01d22c55fcd26032a8357
Ana HG <anaisabel.hortiguela@gmail.com>
2020-01-30 10:14:03 +0000

    Done with Auth

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -46,6 +47,9 @@ const userSchema = new mongoose.Schema({
  })
 
 
+
+
+
  // Instead of storing all the tasks as an array in the user model therefore in MongoDb a virtual property is created.
  userSchema.virtual('tasks', {
      ref: 'Task',
@@ -55,11 +59,11 @@ const userSchema = new mongoose.Schema({
      foreignField:'owner'
  })
 
+
 userSchema.methods.toJSON = function () {
     const user = this
-//  ToObjects returns raw data
-    const userObject= user.toObject()
-    delete userObject.password
+    //  ToObjects returns raw data
+    const userObject = user.toObject()
 
     delete userObject.password
     delete userObject.tokens
@@ -70,51 +74,48 @@ userSchema.methods.toJSON = function () {
 // pre/post an event happens and it has to be a normal function because arrow function don't work with this.
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id: user._id.toString()}, 'thisistopsecret') 
-    user.tokens = user.tokens.concat({token})
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisistopsecret')
+
+    user.tokens = user.tokens.concat({ token })
     await user.save()
-        return token
+
+    return token
 }
 
 // statics are accesible in the models instancesallows to access any  from the Schema directly. 
-userSchema.statics.findByCredentials = async (email, password) =>{
-    const user = await User.findOne({email})
-    if(!user){
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
         throw new Error('Unable to login')
     }
-
     const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
     return user
 }
-
 
 // Hash the plain text password before saving
 userSchema.pre('save', async function(next) {
     const user = this
- 
     if(user.isModified('password')){
         user.password = await bcrypt.hash(user.password, 8)
     }
-
     next()
 })
 
 
- // THE USER MAKER
 
-//  const user = new User({
-//     name: 'Juana',
-//     email: "juana.afe@gmail.com",
-//     password:"jdcs23s",
-//     age: "26"
-// })
-
-// user.save().then(() =>{
-//         console.log(user)
-//     }).catch((error) => {
-//         console.log("User Error. Cause; " + error)
-// }))
+userSchema.pre('remove', async function(next) {
+    const user = this
+    await Task.deleteMany({ owner:user._id })
+    next()
+})
 
 
 const User = mongoose.model('User', userSchema)
+
 module.exports = User
